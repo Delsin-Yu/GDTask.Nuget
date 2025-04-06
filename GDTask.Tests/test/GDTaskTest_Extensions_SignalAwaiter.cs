d95603aed5e3ff32cc8477f186a2b45b4014527c
DE YU <71481700+Delsin-Yu@users.noreply.github.com>
2025-04-06 17:26:19 +0800

    Implement `GDTask.FromSignal` methods from upstream `GDTask.ToSignal` methods.

--- GDTask.Tests/test/GDTaskTest_Extensions_SignalAwaiter.cs
+++ GDTask.Tests/test/GDTaskTest_Extensions_SignalAwaiter.cs
@@ -8,6 +8,75 @@ namespace GodotTask.Tests;
 public partial class GDTaskTest_SignalAwaiter
 {
     [TestCase]
+    public static async Task SignalAwaiter_FromSignal_0Arg()
+    {
+        await GDTask.SwitchToMainThread();
+        var node = Constants.CreateTestNode<SignalTestNode>("SignalTestNode");
+        
+        Constants.Delay()
+            .ContinueWith(node.EmitParam0)
+            .Forget();
+        
+        var result = await GDTask.FromSignal(node, SignalTestNode.SignalName.Param0);
+      
+        node.Free();
+
+        Assertions.AssertThat(result.Length).IsEqual(0);
+    }
+
+    [TestCase]
+    public static async Task SignalAwaiter_FromSignal_2Args()
+    {
+        await GDTask.SwitchToMainThread();
+        var node = Constants.CreateTestNode<SignalTestNode>("SignalTestNode");
+        
+        Constants.Delay()
+            .ContinueWith(() => node.EmitParam2(Constants.ReturnValue, Constants.ReturnValue))
+            .Forget();
+        
+        var (result1, result2) = await GDTask.FromSignal<int, int>(node, SignalTestNode.SignalName.Param2);
+        
+        node.Free();
+
+        Assertions.AssertThat(result1).IsEqual(Constants.ReturnValue);
+        Assertions.AssertThat(result2).IsEqual(Constants.ReturnValue);
+    }
+    
+    [TestCase]
+    public static async Task SignalAwaiter_FromSignal_0Arg_WithCtx()
+    {
+        await GDTask.SwitchToMainThread();
+        var node = Constants.CreateTestNode<SignalTestNode>("SignalTestNode");
+        
+        Constants.Delay()
+            .ContinueWith(node.EmitParam0)
+            .Forget();
+        
+        var (isCanceled, _) = await GDTask.FromSignal(node, SignalTestNode.SignalName.Param0, Constants.CreateCanceledToken()).SuppressCancellationThrow();
+      
+        node.Free();
+
+        Assertions.AssertThat(isCanceled).IsTrue();
+    }
+
+    [TestCase]
+    public static async Task SignalAwaiter_FromSignal_2Args_WithCtx()
+    {
+        await GDTask.SwitchToMainThread();
+        var node = Constants.CreateTestNode<SignalTestNode>("SignalTestNode");
+        
+        Constants.Delay()
+            .ContinueWith(() => node.EmitParam2(Constants.ReturnValue, Constants.ReturnValue))
+            .Forget();
+        
+        var (isCanceled, _) = await GDTask.FromSignal<int, int>(node, SignalTestNode.SignalName.Param2, Constants.CreateCanceledToken()).SuppressCancellationThrow();
+        
+        node.Free();
+
+        Assertions.AssertThat(isCanceled).IsTrue();
+    }    
+    
+    [TestCase]
     public static async Task SignalAwaiter_AsGDTask_0Arg()
     {
         await GDTask.SwitchToMainThread();
@@ -19,7 +88,7 @@ public partial class GDTaskTest_SignalAwaiter
         
         var result = await node.ToSignal(node, SignalTestNode.SignalName.Param0).AsGDTask();
       
-        node.QueueFree();
+        node.Free();
 
         Assertions.AssertThat(result.Length).IsEqual(0);
     }
@@ -36,7 +105,7 @@ public partial class GDTaskTest_SignalAwaiter
         
         var (result1, result2) = await node.ToSignal(node, SignalTestNode.SignalName.Param2).AsGDTask<int, int>();
         
-        node.QueueFree();
+        node.Free();
 
         Assertions.AssertThat(result1).IsEqual(Constants.ReturnValue);
         Assertions.AssertThat(result2).IsEqual(Constants.ReturnValue);
