7c9a8491e301a11d24ccccd30e1edd1654986a6e
DE-YU_Studio <delsin_yu@qq.com>
2026-06-24 15:57:35 +0800

    Update the project to the latest GDUnit and GodotEngine

--- GDTask.Tests/addons/gdUnit4/src/ui/menu/GdUnitEditorFileSystemContextMenuHandler.gd
+++ GDTask.Tests/addons/gdUnit4/src/ui/menu/GdUnitEditorFileSystemContextMenuHandler.gd
@@ -23,21 +23,23 @@ func _init() -> void:
 
 
 func _popup_menu(paths: PackedStringArray) -> void:
-	# Filter for test suites only
-	var test_suites := Array(paths)\
-		.filter(func(file: String) -> bool:
-			return file.get_extension() in ["gd", "cs"])\
-		.filter(func(file: String) -> bool:
-			var script := GdUnitTestSuiteScanner.load_with_disabled_warnings(file)
-			return GdUnitTestSuiteScanner.is_test_suite(script))
+	# Filter for directories and test suite files
+	var valid_paths := Array(paths)\
+		.filter(func(path: String) -> bool:
+			if DirAccess.dir_exists_absolute(path):
+				return true
+			if path.get_extension() in ["gd", "cs"]:
+				var script := GdUnitTestSuiteScanner.load_with_disabled_warnings(path)
+				return GdUnitTestSuiteScanner.is_test_suite(script)
+			return false)
 
-	# If no test suites selected don't extend the context menu
-	if test_suites.is_empty():
+	# If no valid paths selected don't extend the context menu
+	if valid_paths.is_empty():
 		return
 
 	for menu_item in _context_menus:
 		if menu_item.shortcut():
-			add_menu_shortcut(menu_item.shortcut(), menu_item.execute.bindv(test_suites).unbind(1))
+			add_menu_shortcut(menu_item.shortcut(), menu_item.execute.bindv(valid_paths).unbind(1))
 			add_context_menu_item_from_shortcut(menu_item.name, menu_item.shortcut(), menu_item.icon)
 		else:
-			add_context_menu_item(menu_item.name, menu_item.execute.bindv(test_suites).unbind(1), menu_item.icon)
+			add_context_menu_item(menu_item.name, menu_item.execute.bindv(valid_paths).unbind(1), menu_item.icon)
